0785f68dd55fd2ff16c78c2e5dbb2b24a9651487
Lewis <noble.ninja@gmail.com>
2018-03-30 20:45:44 +0100

    making components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+import logo from "./logo.svg";
+import "./App.css";
+
+const middleDotUnicode = "\u00B7";
+
+const SocialCard = styled.div`
+  height: 300px;
+  font-family: Verdana;
+  display: flex;
+  border: solid 2px red;
+`;
+
+const ProfilePicture = styled.img`
+  padding: 15px;
+  height: 40px;
+  border: solid 2px lightblue;
+  border-radius: 50%;
+`;
+
+const Username = styled.h4`
+  padding: 5px;
+`;
+
+const Handle = styled.h6`
+  padding: 5px;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <SocialCard>
+        <ProfilePicture src={logo} alt="logo" />
+        <Username>The Practical Dev</Username>
+        <Handle>@ThePracticalDev {middleDotUnicode} Sep 10</Handle>
+      </SocialCard>
     );
   }
 }
-
 
 export default App;

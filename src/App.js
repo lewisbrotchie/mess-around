f8a4ab3407a447848a9d992ec63be3da57e75359
Lewis <noble.ninja@gmail.com>
2018-03-31 00:00:28 +0100

    creation of components and alignment

--- src/App.js
+++ src/App.js
@@ -5,11 +5,63 @@ import "./App.css";
 
 const middleDotUnicode = "\u00B7";
 
+const Interactions = styled.div`
+  position: absolute;
+  bottom: 0;
+  margin-left: 100px;
+  width: 400px;
+  height: 7%;
+  font-size: 7pt;
+  padding-left: 60%;
+`;
+
+const UserDescription = styled.div`
+  position: absolute;
+  margin-left: 100px;
+  margin-top: 35px;
+  width: 400px;
+  height: 13%;
+  font-size: 9pt;
+`;
+
+const Content = styled.div`
+  margin-left: 100px;
+  margin-bottom: 30px;
+  height: 210px;
+  width: 400px;
+  position: absolute;
+  border: solid 1px #999;
+  bottom: 0;
+  border-radius: 5px;
+`;
+
 const SocialCard = styled.div`
-  height: 300px;
-  font-family: Verdana;
+  height: 320px;
+  width: 550px;
+
+  border: solid 1px #999;
+  border-radius: 10px;
   display: flex;
-  border: solid 2px red;
+  position: relative;
+  font-family: sans-serif;
+  line-height: 24px;
+  text-justify: inter-word;
+`;
+
+const ContentPicture = styled.img`
+  height: 70%;
+  width: 100%;
+  border-radius: 5px;
+  background-color: silver;
+`;
+
+const ContentHeading = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 27%;
+  border-radius: 5px;
 `;
 
 const ProfilePicture = styled.img`
@@ -20,11 +72,18 @@ const ProfilePicture = styled.img`
 `;
 
 const Username = styled.h4`
-  padding: 5px;
+  margin-left: 5px;
+  margin-top: 10px;
+  margin-bottom: 0;
+  height: 10%;
 `;
 
 const Handle = styled.h6`
-  padding: 5px;
+  margin-left: 5px;
+  margin-top: 10px;
+  margin-bottom: 0;
+  height: 10%;
+  color: silver;
 `;
 
 class App extends Component {
@@ -34,6 +93,19 @@ class App extends Component {
         <ProfilePicture src={logo} alt="logo" />
         <Username>The Practical Dev</Username>
         <Handle>@ThePracticalDev {middleDotUnicode} Sep 10</Handle>
+        <UserDescription>User's description goes here...</UserDescription>
+        <Content>
+          <ContentPicture src={logo} alt="content" />
+          <ContentHeading>
+            <h4 Style="padding-left: 5px; margin: 0">
+              Content Title Goes Here..
+            </h4>
+            <h6 Style="padding-left: 5px; margin: 0">
+              Content description goes here...
+            </h6>
+          </ContentHeading>
+        </Content>
+        <Interactions>Comment | Like | Share</Interactions>
       </SocialCard>
     );
   }
